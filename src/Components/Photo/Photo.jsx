52643e8ac66580e38852dcabb012aa165d584cff
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-10 17:47:02 +0600

    single photo component added

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Photo.css'
 
 const Photo = ({ photo }) => {
     const { id, title, url } = photo;
     return (
-        <div>
+        <div className='photo'>
             <h2>Photo Id: {id}</h2>
             <p>Title: {title}</p>
+            <p>Img Url: {url}</p>
         </div>
     );
 };
